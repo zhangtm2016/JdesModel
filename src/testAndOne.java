b094521094d4b3ef8948cec533f751868b8702ba
zhangtm2016 <1476592327@qq.com>
2019-02-15 14:22:31 +0800

    a new start

---
+++ src/testAndOne.java
@@ -0,0 +1,9 @@
+/**
+ * 解释:this is a test
+ * Created by tianming.zhang on 2019/2/15.
+ */
+public class testAndOne {
+    public static void main(String[] args){
+        System.out.print("aaa");
+    }
+}
